86b4ee47949e8276d38b6b583ab71d819feccf27
rajujilla970 <rajujilla970@gmail.com>
2024-12-28 17:14:00 +0530

    Create main.tf

---
+++ terraform/module/VPC/main.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "static-site-vpc"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 1}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
