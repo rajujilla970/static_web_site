95101eabe6e9194fd4ddb0c82a073204147caf67
rajujilla970 <rajujilla970@gmail.com>
2024-12-29 09:17:05 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,15 +6,16 @@ terraform {
       version = "~> 5.49.0"
     }
   }
+provider "aws" {
+  region  = us-east-1
+}
   backend "s3" {
     bucket         = "my-ews-baket1432"
     region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
+    key            = "terraform/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
 }
 
-provider "aws" {
-  region  = var.aws-region
-}
+
